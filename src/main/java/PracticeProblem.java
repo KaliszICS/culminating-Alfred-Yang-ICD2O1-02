abc9b2e8fb5614c716c8f29b7a7ef628d0574bd6
Alfred-Yang-ICD2O1-02 <alfred.yang@student.tdsb.on.ca>
2026-05-27 16:58:12 +0000

    I added a player!

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -9,12 +9,18 @@ import java.util.Scanner;
 public class PracticeProblem {
 	static Scanner input = new Scanner(System.in); 
 	public static void main(String args[]) {
-		
-		System.out.println("[1] Start Uno.\n[2] Rules.\nPlease enter one of the above: ");
-		String userInput = input.nextLine();
+		String userInput = "";
+		while (!(userInput.equals("1") || (userInput.equals("2")))){
+		System.out.print("[1] Start Uno.\n[2] Rules.\nPlease enter one of the above: ");
+		userInput = input.nextLine();
 		if (userInput.equals("1")){
-
+			players();
 		} 
+		else{
+			System.out.print("Invalid Input, please input an option above\nPress enter to continue: ");
+			input.nextLine();
+		}
+		}
 	}
 
 	public static String[] Cards(){
@@ -28,6 +34,26 @@ public class PracticeProblem {
 		everyCard += "Red,$ Blue,$ Yellow,$ Green,$ Red,% Blue,% Yellow,% Green,% Red,+2 Blue,+2 Yellow,+2 Green,+2 Wild Wild Wild Wild Wild,+4 Wild,+4 Wild,+4 Wild,+4";
 		String[] cards = everyCard.split(" ");
 		return cards;
+	}
+
+	public static String[] players(){
+		String playercount = "";
+		System.out.print("Pick a range of players from 2-4: ");
+		playercount = input.nextLine();
+		while (!(playercount.equals("2") || playercount.equals("3") || playercount.equals("4"))){
+			System.out.print("Invalid Input, please put a number from 2-4\nPress enter to continue: ");
+			input.nextLine();
+			System.out.print("Pick a range of players from 2-4: ");
+			playercount = input.nextLine();
+		}
+		int numberOfPlayers = Integer.parseInt(playercount);
+		String[] playersNames = new String[numberOfPlayers];
+
+		for (int i = 0; i < numberOfPlayers; i++){
+			System.out.println("Select a player name for player " + playercount + ": ");
+			playersNames [i] = input.nextLine();
+		}
+		return playersNames;
 	}
 
 }
