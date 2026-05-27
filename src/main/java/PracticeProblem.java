0a4f20d94df51aaf2b5a39e04396d312e53ec063
Alfred-Yang-ICD2O1-02 <alfred.yang@student.tdsb.on.ca>
2026-05-27 16:28:25 +0000

    ih

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 /**
         * File: Culminating
         * Author: Alfred Yang
@@ -7,9 +5,11 @@ import java.util.ArrayList;
         * Date Last Modified: June ????????????????????????????????????/, 2026
         */
 import java.util.ArrayList;
+import java.util.Scanner;
 public class PracticeProblem {
+	static Scanner input = new Scanner(System.in); 
 	public static void main(String args[]) {
-
+		//Creating Cards
 		String everyCard = "";	
 		for (int i = 0; i < 10; i++){
 			everyCard += "Red," + i + " ";
@@ -19,10 +19,11 @@ public class PracticeProblem {
 		}
 		everyCard += "Red,$ Blue,$ Yellow,$ Green,$ Red,% Blue,% Yellow,% Green,% Red,+2 Blue,+2 Yellow,+2 Green,+2 Wild Wild Wild Wild Wild,+4 Wild,+4 Wild,+4 Wild,+4";
 		String[] cards = everyCard.split(" ");
-		for (int i = 0; i < 109; i++){
-					System.out.println(cards[i]);
 
-		}
+		System.out.println("[1] Start Uno.\n[2] Rules.\nPlease enter one of the above: ");
+		String userInput = input.nextLine();
+		if (userInput.equals("1")){
+			
+		} 
 	}
-
 }
