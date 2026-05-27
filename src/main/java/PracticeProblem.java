d298c10087ab22f3b88e044b825cc22270e28900
Alfred-Yang-ICD2O1-02 <alfred.yang@student.tdsb.on.ca>
2026-05-27 16:02:35 +0000

    gwaib

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,22 @@
-public class PracticeProblem {
+/**
+        * File: Culminating
+        * Author: Alfred Yang
+        * Date Created: May 27, 2026
+        * Date Last Modified: June ????????????????????????????????????/, 2026
+        */
 
+public class PracticeProblem {
 	public static void main(String args[]) {
 
-	}
-
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+		String everyCard = "";	
+		for (int i = 0; i < 10; i++){
+			everyCard += "Red," + i + "|";
+			everyCard += "Blue," + i + "|";
+			everyCard += "Yellow," + i + "|";
+			everyCard += "Green," + i + "|";
+		}
+		everyCard += "Red, x|Blue, x|Yellow, x|Green, x";
+		System.out.println(everyCard);
 	}
 
 }
